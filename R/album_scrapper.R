28c72bb6963b6e5c4db9bbf1b740f8fd7f311621
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-10-10 20:53:16 -0300

    refatora album scrapper

--- R/album_scrapper.R
+++ R/album_scrapper.R
@@ -2,8 +2,11 @@ source("./R/constants.R")
 library(RSelenium)
 library(readr)
 
-scrap_album <- function(year) {
-  remDr <- .open_remDr()
+#' @title Extract best albums by year
+#' @param remDr Remote driver
+#' @param year Year of albums
+#' @return Return list with object of scrapper
+scrap_album <- function(remDr, year) {
   remDr$navigate(paste0(URL_ALBUMS, year))
 
   element <- remDr$findElement(using = 'class', value = 'list_products')
@@ -13,8 +16,11 @@ scrap_album <- function(year) {
   return(result)
 }
 
-create_best_albums <- function(result_scrap) {
-  matrix <- matrix(matrix(unlist(result_scrap), ncol=5, byrow=T))
+#' @title Create dataframe with top 100 best albums
+#' @param obj_scrap Object scrapper
+#' @return Dataframe with best albums
+create_best_albums <- function(obj_scrap) {
+  matrix <- matrix(matrix(unlist(obj_scrap), ncol=5, byrow=T))
 
   name <- matrix[1:100]
   metascore <- matrix[101:200]
@@ -23,20 +29,38 @@ create_best_albums <- function(result_scrap) {
   release_date <- matrix[401:500]
 
   best_albums <- data.frame(name, metascore, artist_name, user_score, release_date)
-  return(best_albuns)
+  return(best_albums)
 }
 
+#' @title Execute scrapper by year
+#' @param year Year of albums
+run_scrap <- function(year) {
+  remDr <- .open_remDr()
+  scrap <- scrap_album(remDr, year)
+  df_best_albums <- create_best_albums(scrap)
+  .create_table_best_albuns(df_best_albums)
+  .close_remDr(remDr)
+
+}
+
+#' @title Create table with the best albums
+#' @param df_best_albums Dataframe
+.create_table_best_albuns <- function(df_best_albums) {
+  write_csv(df_best_albums, "data/best_albums_2.csv")
+}
+
+#' @title Open to connect to remote server
+#' @return remDr Object remote driver
 .open_remDr <- function() {
   remDr <- remoteDriver(port = PORT)
-  result <- remDr$open()
+  remDr$open()
   return(remDr)
 }
 
+#' @title Close to connect remote server
+#' @param remDr Object remote driver
 .close_remDr <- function(remDr) {
   remDr$close()
 }
 
-create_table_best_albuns <- function(df_best_albums) {
-  write_csv(df_best_albums, "data/best_albums.csv")
-}
 
